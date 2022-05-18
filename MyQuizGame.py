18c583e68e1e16a819cb1a505e71e99ddab47f12
john pitts <zennet57@gmail.com>
2022-05-18 18:34:42 -0400

    game quiz

---
+++ MyQuizGame.py
@@ -0,0 +1,51 @@
+print("welcom to my computer quiz!")
+
+playing = input("do you want to play? ")
+
+if playing  != "yes" :
+    quit()
+
+print("Okay! Let's play :)")
+score = 0
+
+answer = input("What does CPU stand for? ")
+if answer.lower() == "central processing unit":
+    print('Correct!')
+    score += 1
+    score = score+ 1
+else:
+        print("Incorrect!")
+
+answer = input("What does GPU stand for? ")
+if answer.lower() == "graphics processing unit":
+    print('Correct!')
+    score += 1
+    score = score+ 1
+else:
+        print("Incorrect!")
+
+answer = input("What does RAm stand for? ")
+if answer.lower() == "random access memory":
+    print('Correct!')
+    score += 1
+    score = score+ 1
+else:
+        print("Incorrect!")
+
+answer = input("What does PSU stand for? ")
+if answer.lower() == "power supply":
+    print('Correct!')
+    score += 1
+    score = score+ 1
+else:
+        print("Incorrect!")
+
+
+answer = input("What does DPS stand for? ")
+if answer.lower() == "damage per second":
+    print('Correct!')
+    score += 1
+    score = score+ 1
+else:
+        print("Incorrect!")
+        
